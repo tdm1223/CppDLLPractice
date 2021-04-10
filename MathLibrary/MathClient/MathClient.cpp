6565a8401c862ae35e49d566735d9baa1c56efa4
Jegyun Kang <tdm1223@naver.com>
2021-04-10 13:28:40 +0900

    [UPDATE] MathLibrary 같은 프로젝트에서 실행할 수 있는 클라이언트 추가

---
+++ MathLibrary/MathClient/MathClient.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+#include "MathLibrary.h"
+int main()
+{
+    // 피보나치 시퀀스 초기화
+    fibonacci_init(1, 1);
+
+    // overflow가 일어날때까지 피보나치 출력
+    do {
+        std::cout << fibonacci_index() << ": " << fibonacci_current() << std::endl;
+    } while (fibonacci_next());
+    std::cout << fibonacci_index() + 1 <<" Fibonacci sequence values fit in an unsigned 64-bit integer." << std::endl;
+    while (1);
+    return 0;
+}
